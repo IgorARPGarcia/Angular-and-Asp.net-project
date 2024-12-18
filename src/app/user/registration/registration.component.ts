19222ee322f19368a67adc48396916b01af8a7ca
igor <higoralves112@gmail.com>
2024-12-18 10:44:09 -0300

    v1.0 front-end

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
-export class RegistrationComponent {
 
+export class RegistrationComponent {
+  form : any;
+
+  constructor(public formBuilder : FormBuilder){
+    this.form = this.formBuilder.group({
+      fullName : [""],
+      email : [""],
+      password : [""],
+      confirmPassword : [""]
+    })
+  }
+
+  onSubmit(){
+    console.log(this.form.value);
+  }
+  
 }
+
